a223021df5d754beca93a1a46e423ae8ccd147e6
daniella-melo <fcmdaniella@gmail.com>
2019-05-10 19:17:26 -0300

    Integrando JSF com SpringBoot

---
+++ src/main/java/com/daniella/Etecs/EtecsApplication.java
@@ -0,0 +1,13 @@
+package com.daniella.Etecs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtecsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtecsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daniella/Etecs/EtecsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.daniella.Etecs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EtecsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
