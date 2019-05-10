c94193777d4366bea5db094497f1829846b19415
daniella-melo <fcmdaniella@gmail.com>
2019-05-10 19:31:45 -0300

    Configurações Gerais + Banco

--- src/main/java/com/daniella/Etecs/EtecsApplication.java
+++ src/main/java/com/daniella/Etecs/EtecsApplication.java
@@ -1,13 +1,22 @@
 package com.daniella.Etecs;
 
+import javax.faces.webapp.FacesServlet;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EtecsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EtecsApplication.class, args);
+	}
+	
+	@Bean
+	public ServletRegistrationBean<FacesServlet> servletRegistrationBean() {
+		return new ServletRegistrationBean<FacesServlet>(new FacesServlet(), "*.xhtml");
 	}
 
 }
